b388868a39ee1cd7a349962db35bf16fec2b8b22
thefloppydriver <thefloppydriver@gmail.com>
2022-03-03 12:15:45 -0500

    Delete stage-0-start-script.sh

--- stage-0-start-script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-chmod 775 ./stage-1-kernel-patch.sh
-chmod 775 ./stage-2-build-modules.sh
-chmod 775 ./stage-3-pair-with-wiiu.py
-chmod 775 ./stage-4-start-pc2drc.py
-
-echo "Good to go!"
-echo 'To start a script, type sudo -E ./bash-script-name.sh     OR     sudo -E python3 ./python-script-name.py'
